c4de5c7d89be0cb9ccceacd56a8376be6a31eec4
Rajeshwari Kondian <rajeshwarikondian@gmail.com>
2025-08-02 21:13:43 +0530

    last changes

--- server.js
+++ server.js
@@ -1,42 +1,43 @@
-import express from 'express'
-import cors from 'cors';
-import 'dotenv/config';
-import cookieParser from 'cookie-parser';
-import connectDB from './config/mongodb.js';
-import authRouter from './routes/authRoutes.js';
-import userRouter from './routes/userRoutes.js';
+  import express from 'express'
+  import cors from 'cors';
+  import 'dotenv/config';
+  import cookieParser from 'cookie-parser';
+  import connectDB from './config/mongodb.js';
+  import authRouter from './routes/authRoutes.js';
+  import userRouter from './routes/userRoutes.js';
 
 
-const app = express()
-const port = process.env.PORT || 4000
+  const app = express()
+  const port = process.env.PORT || 4000
 
-// Connect MongoDB
-connectDB();
+  // Connect MongoDB
+  connectDB();
 
-// Allow frontend origin for API access
-const allowedOrigins = ['http://localhost:5173',
-  "https://mern-auth-frontend-five-chi.vercel.app/"
-]
+  // Allow frontend origin for API access
+  const allowedOrigins = ['http://localhost:5173',
+    "https://mern-auth-frontend-five-chi.vercel.app",
 
-
-// middleware
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({ origin:allowedOrigins, 
-credentials: true }));
+  ]
 
 
-
-//API endpoints
-app.get("/", (req, res) => {
-  res.send("MERN Auth Backend is Live 🚀");
-});
-app.use("/api/auth", authRouter);
-app.use("/api/user",userRouter)
+  // middleware
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(cors({ origin:allowedOrigins, 
+  credentials: true }));
 
 
 
-//server listener or server binding
-app.listen(port,()=>{
-    console.log(`server running in http://localhost:${port}`)
-})
+  //API endpoints
+  app.get("/", (req, res) => {
+    res.send("MERN Auth Backend is Live 🚀");
+  });
+  app.use("/api/auth", authRouter);
+  app.use("/api/user",userRouter)
+
+
+
+  //server listener or server binding
+  app.listen(port,()=>{
+      console.log(`server running in http://localhost:${port}`)
+  })
